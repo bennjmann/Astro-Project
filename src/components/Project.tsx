c0a0861e6b4f30959da082bde09f77a3192bc9c0
Squabbit <bennjmann.red@gmail.com>
2023-11-09 00:05:35 +1000

    Added project componet
    Removed Cards from index.
    Added Project component.
    --Added project.tsx
    --Added project.module.css
    
    .sectionContent will need to be reworked Later on.

---
+++ src/components/Project.tsx
@@ -0,0 +1,39 @@
+import React, {type ReactNode} from 'react';
+import Style from "../styles/project.module.css";
+
+interface Props {
+    alt: string;
+    children: ReactNode;
+    date?: Date;
+    github?: string;
+    link?: string;
+    title: string;
+    type: string;
+    src?: string;
+}
+
+const Project = ({alt, children, date, github, link, title, type, src} : Props) => {
+    return (
+       <div className={Style.container}>
+           <div>
+               <h3 className={Style.title}>{title}</h3>
+               <div className={Style.categoriesContainer}>
+                   <div>Completion Year:<br/> {date?.toLocaleDateString()}  </div>
+                   <div>Project Type:<br/> {type}</div>
+               </div>
+               <div className={Style.descContainer}>
+                   {children}
+               </div>
+               <div className={Style.buttonContainer}>
+                   { github ?  <a href={github}>GitHub Repo</a> : null }
+                   { link ? <a href={link}>Live Project</a> : null }
+               </div>
+           </div>
+           <div className={Style.imgContainer}>
+               <img src={src} alt={alt} width={375} height={225}></img>
+           </div>
+       </div>
+    );
+};
+
+export default Project;
